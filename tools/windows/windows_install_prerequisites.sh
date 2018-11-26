RogoSolutions/rogo-esp32-sdk

commit a564231b841a62843415e82f4a0c34d9707e85ed

docs, scripts: Correct the priority of the PATH setting

tools/windows/windows_install_prerequisites.sh
@@ -54,7 +54,7 @@ rm ~/${TOOLCHAIN_ZIP}
 cat > /etc/profile.d/esp32_toolchain.sh << EOF
 # This file was created by ESP-IDF windows_install_prerequisites.sh
 # and will be overwritten if that script is run again.
-export PATH="\$PATH:/opt/xtensa-esp32-elf/bin"
+export PATH="\/opt/xtensa-esp32-elf/bin:$PATH"
 EOF
 
 # clean up pacman package cache to save some disk space

add_path.sh
@@ -13,7 +13,7 @@ else
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/espcoredump"
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/partition_table/"
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/tools/"
-	export PATH="${PATH}:${IDF_ADD_PATHS_EXTRAS}"
+	export PATH="${IDF_ADD_PATHS_EXTRAS}:${PATH}"
 	echo "Added to PATH: ${IDF_ADD_PATHS_EXTRAS}"
 fi
 

commit c7da1f961bbd673cf16f48fe473e32c5367f187f

windows_install_prerequisites: Fix generation of PATH command
Accidental regression in a564231b841a62843415e82f4a0c34d9707e85ed - escape
character didn't move with PATH.

Reported on forum https://esp32.com/viewtopic.php?f=13&t=8201

tools/windows/windows_install_prerequisites.sh
@@ -54,7 +54,7 @@ rm ~/${TOOLCHAIN_ZIP}
 cat > /etc/profile.d/esp32_toolchain.sh << EOF
 # This file was created by ESP-IDF windows_install_prerequisites.sh
 # and will be overwritten if that script is run again.
-export PATH="\/opt/xtensa-esp32-elf/bin:$PATH"
+export PATH="/opt/xtensa-esp32-elf/bin:\$PATH"
 EOF
 
 # clean up pacman package cache to save some disk space