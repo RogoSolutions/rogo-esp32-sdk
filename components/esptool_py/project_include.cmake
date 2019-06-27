RogoSolutions/rogo-esp32-sdk

commit bc920c06fc7a56f89cf250817fb6725b2b7a454e

esptool_py: use passed offset and image when template is given
esptool_py defines command `esptool_py_flash_project_args` that
generates arg file for esptool.py. Two of the arguments are the offset
and image, which are not being used when a template file is given.
This commit makes variables OFFSET and IMAGE available to the template
file, which will holds the value of the offset and image arguments to
`esptool_py_flash_project_args`.

components/esptool_py/project_include.cmake
@@ -148,7 +148,10 @@ add_custom_target(flash_project_args_target)
 
 # esptool_py_flash_project_args
 #
-# Add file to the flasher args list, to be flashed at a particular offset
+# Add file to the flasher args list, to be flashed at a particular offset.
+#
+# When a template FLASH_FILE_TEMPLATE is given, the variables OFFSET and IMAGE
+# hold the value of arguments offset and image, respectively.
 function(esptool_py_flash_project_args entry offset image)
     set(options FLASH_IN_PROJECT)  # flash the image when flashing the project
     set(single_value FLASH_FILE_TEMPLATE) # template file to use to be able to
@@ -172,8 +175,16 @@ function(esptool_py_flash_project_args entry offset image)
     if(NOT __FLASH_FILE_TEMPLATE)
         file(GENERATE OUTPUT ${entry_flash_args} CONTENT "${offset} ${image}")
     else()
+        set(OFFSET ${offset})
+        set(IMAGE ${image})
         get_filename_component(template "${__FLASH_FILE_TEMPLATE}" ABSOLUTE)
-        file(GENERATE OUTPUT ${entry_flash_args} INPUT ${template})
+        configure_file(${template} ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2)
+        file(GENERATE OUTPUT ${entry_flash_args} INPUT ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2)
+        set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    APPEND PROPERTY
+                    ADDITIONAL_MAKE_CLEAN_FILES ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2})
+        unset(OFFSET)
+        unset(IMAGE)
     endif()
 
     set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}

components/bootloader/CMakeLists.txt
@@ -7,8 +7,7 @@ endif()
 
 # Set values used in flash_bootloader_args.in and generate flash file
 # for bootloader
-set(BOOTLOADER_OFFSET 0x1000)
-esptool_py_flash_project_args(bootloader ${BOOTLOADER_OFFSET}
+esptool_py_flash_project_args(bootloader 0x1000
                             ${BOOTLOADER_BUILD_DIR}/bootloader.bin
                             FLASH_IN_PROJECT
                             FLASH_FILE_TEMPLATE flash_bootloader_args.in)

components/bootloader/flash_bootloader_args.in
@@ -1,4 +1,4 @@
 --flash_mode ${ESPFLASHMODE}
 --flash_size ${ESPFLASHSIZE}
 --flash_freq ${ESPFLASHFREQ}
-${BOOTLOADER_OFFSET} bootloader/bootloader.bin 
+${OFFSET} ${IMAGE}

commit 097d8a813b171385d793505f2b455de2ff2898d9

esptool+cmake: Fix path generation for args.in2 file
Path was accidentally generated as build/<absolute path to input file> which creates unexpected path structure on Linux/macOS and breaks Windows (as can't have a directory named "C:".

Regression in bc920c06fc7a56f89cf250817fb6725b2b7a454e

Closes https://github.com/espressif/esp-idf/issues/3687
Closes IDFGH-1409

components/esptool_py/project_include.cmake
@@ -183,12 +183,16 @@ function(esptool_py_flash_project_args entry offset image)
     else()
         set(OFFSET ${offset})
         set(IMAGE ${image})
-        get_filename_component(template "${__FLASH_FILE_TEMPLATE}" ABSOLUTE)
-        configure_file(${template} ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2)
-        file(GENERATE OUTPUT ${entry_flash_args} INPUT ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2)
+
+        get_filename_component(template_in "${__FLASH_FILE_TEMPLATE}" ABSOLUTE)
+        get_filename_component(template_name "${template_in}" NAME)
+        set(template_partial "${CMAKE_CURRENT_BINARY_DIR}/${template_name}.in2")
+
+        configure_file("${template_in}" "${template_partial}")
+        file(GENERATE OUTPUT ${entry_flash_args} INPUT "${template_partial}")
         set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     APPEND PROPERTY
-                    ADDITIONAL_MAKE_CLEAN_FILES ${CMAKE_CURRENT_BINARY_DIR}/${template}.in2})
+                    ADDITIONAL_MAKE_CLEAN_FILES "${template_partial}")
         unset(OFFSET)
         unset(IMAGE)
     endif()