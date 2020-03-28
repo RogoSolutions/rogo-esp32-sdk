RogoSolutions/rogo-esp32-sdk

commit 2bfade8637767035193020cca8dcb6756eb863af

socket examples: add tests for server and client applications

examples/protocols/sockets/udp_client/main/udp_client.c
@@ -23,12 +23,14 @@
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
+#include "addr_from_stdin.h"
 
-
-#ifdef CONFIG_EXAMPLE_IPV4
+#if defined(CONFIG_EXAMPLE_IPV4)
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
-#else
+#elif defined(CONFIG_EXAMPLE_IPV6)
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#else
+#define HOST_IP_ADDR ""
 #endif
 
 #define PORT CONFIG_EXAMPLE_PORT
@@ -40,13 +42,13 @@ static const char *payload = "Message from ESP32 ";
 static void udp_client_task(void *pvParameters)
 {
     char rx_buffer[128];
-    char addr_str[128];
-    int addr_family;
-    int ip_protocol;
+    char host_ip[] = HOST_IP_ADDR;
+    int addr_family = 0;
+    int ip_protocol = 0;
 
     while (1) {
 
-#ifdef CONFIG_EXAMPLE_IPV4
+#if defined(CONFIG_EXAMPLE_IPV4)
         struct sockaddr_in dest_addr;
         dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
         dest_addr.sin_family = AF_INET;
@@ -54,7 +56,7 @@ static void udp_client_task(void *pvParameters)
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
         inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
-#else // IPV6
+#elif defined(CONFIG_EXAMPLE_IPV6)
         struct sockaddr_in6 dest_addr = { 0 };
         inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
         dest_addr.sin6_family = AF_INET6;
@@ -63,6 +65,9 @@ static void udp_client_task(void *pvParameters)
         addr_family = AF_INET6;
         ip_protocol = IPPROTO_IPV6;
         inet6_ntoa_r(dest_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_in6 dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
 #endif
 
         int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
@@ -93,8 +98,12 @@ static void udp_client_task(void *pvParameters)
             // Data received
             else {
                 rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
                 ESP_LOGI(TAG, "%s", rx_buffer);
+                if (strncmp(rx_buffer, "OK: ", 4) == 0) {
+                    ESP_LOGI(TAG, "Received expected message, reconnecting");
+                    break;
+                }
             }
 
             vTaskDelay(2000 / portTICK_PERIOD_MS);

examples/protocols/sockets/scripts/tcpclient.py
@@ -1,54 +1,0 @@
-# This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-# Unless required by applicable law or agreed to in writing, this
-# software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied.
-
-# -*- coding: utf-8 -*-
-
-from builtins import input
-import socket
-import sys
-
-# -----------  Config  ----------
-PORT = 3333
-IP_VERSION = 'IPv6'
-IPV4 = '192.168.0.42'
-IPV6 = 'fd00:0000:0000:0000:260a:c4ff:fe09:885c'
-# -------------------------------
-
-if IP_VERSION == 'IPv4':
-    family_addr = socket.AF_INET
-    addr = (IPV4, PORT)
-elif IP_VERSION == 'IPv6':
-    family_addr = socket.AF_INET6
-    for res in socket.getaddrinfo(IPV6, PORT, socket.AF_INET6,
-                                  socket.SOCK_STREAM, socket.SOL_TCP):
-        af, socktype, proto, canonname, addr = res
-else:
-    print('IP_VERSION must be IPv4 or IPv6')
-    sys.exit(1)
-
-try:
-    sock = socket.socket(family_addr, socket.SOCK_STREAM)
-except socket.error as msg:
-        print('Could not create socket: ' + str(msg[0]) + ': ' + msg[1])
-        sys.exit(1)
-
-try:
-    sock.connect((IPV6, PORT))
-except socket.error as msg:
-        print('Could not open socket: ', msg)
-        sock.close()
-        sys.exit(1)
-
-while True:
-    msg = input('Enter message to send: ')
-    assert isinstance(msg, str)
-    msg = msg.encode()
-    sock.sendall(msg)
-    data = sock.recv(1024)
-    if not data:
-        break
-    print('Reply: ' + data.decode())
-sock.close()

examples/protocols/sockets/scripts/tcpserver.py
@@ -1,54 +1,0 @@
-# This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-# Unless required by applicable law or agreed to in writing, this
-# software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied.
-
-# -*- coding: utf-8 -*-
-
-import socket
-import sys
-
-# -----------  Config  ----------
-IP_VERSION = 'IPv4'
-PORT = 3333
-# -------------------------------
-
-if IP_VERSION == 'IPv4':
-    family_addr = socket.AF_INET
-elif IP_VERSION == 'IPv6':
-    family_addr = socket.AF_INET6
-else:
-    print('IP_VERSION must be IPv4 or IPv6')
-    sys.exit(1)
-
-
-try:
-    sock = socket.socket(family_addr, socket.SOCK_STREAM)
-except socket.error as msg:
-    print('Error: ' + str(msg[0]) + ': ' + msg[1])
-    sys.exit(1)
-
-print('Socket created')
-
-try:
-    sock.bind(('', PORT))
-    print('Socket binded')
-    sock.listen(1)
-    print('Socket listening')
-    conn, addr = sock.accept()
-    print('Connected by', addr)
-except socket.error as msg:
-    print('Error: ' + str(msg[0]) + ': ' + msg[1])
-    sock.close()
-    sys.exit(1)
-
-while True:
-    data = conn.recv(128)
-    if not data:
-        break
-    data = data.decode()
-    print('Received data: ' + data)
-    reply = 'OK: ' + data
-    conn.send(reply.encode())
-conn.close()

examples/protocols/sockets/scripts/udpclient.py
@@ -1,49 +1,0 @@
-# This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-# Unless required by applicable law or agreed to in writing, this
-# software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied.
-
-# -*- coding: utf-8 -*-
-
-from builtins import input
-import socket
-import sys
-
-# -----------  Config  ----------
-PORT = 3333
-IP_VERSION = 'IPv6'
-IPV4 = '192.168.0.167'
-IPV6 = 'fe80:0000:0000:0000:260a:c4ff:fe11:a1e0%wlp1s0'
-# -------------------------------
-
-if IP_VERSION == 'IPv4':
-    addr = (IPV4, PORT)
-    family_addr = socket.AF_INET
-elif IP_VERSION == 'IPv6':
-    family_addr = socket.AF_INET6
-    for res in socket.getaddrinfo(IPV6, PORT, socket.AF_INET6,
-                                  socket.SOCK_DGRAM, socket.SOL_UDP):
-        af, socktype, proto, canonname, addr = res
-else:
-    print('IP_VERSION must be IPv4 or IPv6')
-    sys.exit(1)
-
-
-try:
-    sock = socket.socket(family_addr, socket.SOCK_DGRAM)
-except socket.error:
-    print('Failed to create socket')
-    sys.exit()
-
-while True:
-    msg = input('Enter message to send : ')
-    try:
-        sock.sendto(msg.encode(), addr)
-        reply, addr = sock.recvfrom(128)
-        if not reply:
-            break
-        print('Reply[' + addr[0] + ':' + str(addr[1]) + '] - ' + str(reply))
-    except socket.error as msg:
-        print('Error Code : ' + str(msg[0]) + ' Message: ' + msg[1])
-        sys.exit()

examples/protocols/sockets/scripts/udpserver.py
@@ -1,52 +1,0 @@
-# This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-# Unless required by applicable law or agreed to in writing, this
-# software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied.
-
-# -*- coding: utf-8 -*-
-
-import socket
-import sys
-
-# -----------  Config  ----------
-IP_VERSION = 'IPv4'
-PORT = 3333
-# -------------------------------
-
-if IP_VERSION == 'IPv4':
-    family_addr = socket.AF_INET
-elif IP_VERSION == 'IPv6':
-    family_addr = socket.AF_INET6
-else:
-    print('IP_VERSION must be IPv4 or IPv6')
-    sys.exit(1)
-
-
-try:
-    sock = socket.socket(family_addr, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-except socket.error as msg:
-    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-
-try:
-    sock.bind(('', PORT))
-except socket.error as msg:
-    print('Bind failed. Error: ' + str(msg[0]) + ': ' + msg[1])
-    sys.exit()
-
-while True:
-    try:
-        print('Waiting for data...')
-        data, addr = sock.recvfrom(1024)
-        if not data:
-            break
-        data = data.decode()
-        print('Reply[' + addr[0] + ':' + str(addr[1]) + '] - ' + data)
-        reply = 'OK ' + data
-        sock.sendto(reply.encode(), addr)
-    except socket.error as msg:
-        print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-
-sock.close()

examples/protocols/sockets/socket_addr_from_stdin/CMakeLists.txt
@@ -1,0 +1,3 @@
+idf_component_register(SRCS "addr_from_stdin.c"
+                    INCLUDE_DIRS "include"
+                    PRIV_REQUIRES protocol_examples_common)

examples/protocols/sockets/socket_addr_from_stdin/addr_from_stdin.c
@@ -1,0 +1,65 @@
+#include <string.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "lwip/sockets.h"
+#include <lwip/netdb.h>
+#include <arpa/inet.h>
+
+#define HOST_IP_SIZE 128
+
+esp_err_t get_addr_from_stdin(int port, int sock_type, int *ip_protocol, int *addr_family, struct sockaddr_in6 *dest_addr)
+{
+    char host_ip[HOST_IP_SIZE];
+    int len;
+    static bool already_init = false;
+
+    // this function could be called multiple times -> make sure UART init runs only once
+    if (!already_init) {
+        example_configure_stdin_stdout();
+        already_init = true;
+    }
+
+    // ignore empty or LF only string (could receive from DUT class)
+    do {
+        fgets(host_ip, HOST_IP_SIZE, stdin);
+        len = strlen(host_ip);
+    } while (len<=1 && host_ip[0] == '\n');
+    host_ip[len - 1] = '\0';
+
+    struct addrinfo hints, *addr_list, *cur;
+    memset( &hints, 0, sizeof( hints ) );
+
+    // run getaddrinfo() to decide on the IP protocol
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = sock_type;
+    hints.ai_protocol = IPPROTO_TCP;
+    if( getaddrinfo( host_ip, NULL, &hints, &addr_list ) != 0 ) {
+        return ESP_FAIL;
+    }
+    for( cur = addr_list; cur != NULL; cur = cur->ai_next ) {
+        memcpy(dest_addr, cur->ai_addr, sizeof(*dest_addr));
+        if (cur->ai_family == AF_INET) {
+            *ip_protocol = IPPROTO_IP;
+            *addr_family = AF_INET;
+            // add port number and return on first IPv4 match
+            ((struct sockaddr_in*)dest_addr)->sin_port = htons(port);
+            freeaddrinfo( addr_list );
+            return ESP_OK;
+
+        } else if (cur->ai_family == AF_INET6) {
+            *ip_protocol = IPPROTO_IPV6;
+            *addr_family = AF_INET6;
+            // add port and interface number and return on first IPv6 match
+            dest_addr->sin6_port = htons(port);
+            dest_addr->sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+            freeaddrinfo( addr_list );
+            return ESP_OK;
+        }
+    }
+    // no match found
+    freeaddrinfo( addr_list );
+    return ESP_FAIL;
+}

examples/protocols/sockets/socket_addr_from_stdin/component.mk
@@ -1,0 +1,5 @@
+#
+# Component Makefile
+#
+COMPONENT_ADD_INCLUDEDIRS := include
+COMPONENT_SRCDIRS := .

commit 64b9f40d728ea4e23f654688f9a4c9f73d0a1282

socket examples: Fix udp_client build error
Fix below build error:
../main/udp_client.c: In function 'udp_client_task':
../main/udp_client.c:58:41: error: 'addr_str' undeclared (first use in this function); did you mean 'caddr_t'?

Fixes: 2bfade863776 ("socket examples: add tests for server and client applications")
Signed-off-by: Axel Lin <axel.lin@gmail.com>

examples/protocols/sockets/udp_client/main/udp_client.c
@@ -55,7 +55,6 @@ static void udp_client_task(void *pvParameters)
         dest_addr.sin_port = htons(PORT);
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
 #elif defined(CONFIG_EXAMPLE_IPV6)
         struct sockaddr_in6 dest_addr = { 0 };
         inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
@@ -64,7 +63,6 @@ static void udp_client_task(void *pvParameters)
         dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
         addr_family = AF_INET6;
         ip_protocol = IPPROTO_IPV6;
-        inet6_ntoa_r(dest_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
 #elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
         struct sockaddr_in6 dest_addr = { 0 };
         ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));