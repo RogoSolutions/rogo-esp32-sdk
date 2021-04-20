RogoSolutions/rogo-esp32-sdk

commit 06225f982d28c82cf3c03c38d46f1aba56caeddb

esp_https_ota: Enable option of using global_ca_store and x509_crt_bundle Closes https://github.com/espressif/esp-idf/issues/5504

components/esp_https_ota/src/esp_https_ota.c
@@ -162,6 +162,12 @@ static esp_err_t _ota_write(esp_https_ota_t *https_ota_handle, const void *buffe
     return err;
 }
 
+static bool is_server_verification_enabled(esp_https_ota_config_t *ota_config) {
+    return  (ota_config->http_config->cert_pem
+            || ota_config->http_config->use_global_ca_store
+            || !(ota_config->http_config->crt_bundle_attach == NULL));
+}
+
 esp_err_t esp_https_ota_begin(esp_https_ota_config_t *ota_config, esp_https_ota_handle_t *handle)
 {
     esp_err_t err;
@@ -175,8 +181,8 @@ esp_err_t esp_https_ota_begin(esp_https_ota_config_t *ota_config, esp_https_ota_
     }
 
 #if !CONFIG_OTA_ALLOW_HTTP
-    if (!ota_config->http_config->cert_pem) {
-        ESP_LOGE(TAG, "Server certificate not found in esp_http_client config");
+    if (!is_server_verification_enabled(ota_config)) {
+        ESP_LOGE(TAG, "No option for server verification is enabled in esp_http_client config.");
         *handle = NULL;
         return ESP_ERR_INVALID_ARG;
     }

commit 89b74b2c0e6c422b510368c9dbe8b50071352fcf

esp_https_ota: Fix build warning when CONFIG_OTA_ALLOW_HTTP is set
Fix below build warning when CONFIG_OTA_ALLOW_HTTP is set:
esp_https_ota.c:165:13: warning: 'is_server_verification_enabled' defined but not used [-Wunused-function]

While at it, also simplify the is_server_verification_enabled logic.

Fixes: 06225f982d28 ("esp_https_ota: Enable option of using global_ca_store and x509_crt_bundle")
Signed-off-by: Axel Lin <axel.lin@gmail.com>

Closes https://github.com/espressif/esp-idf/pull/6882

Signed-off-by: Aditya Patwardhan <aditya.patwardhan@espressif.com>

components/esp_https_ota/src/esp_https_ota.c
@@ -162,11 +162,13 @@ static esp_err_t _ota_write(esp_https_ota_t *https_ota_handle, const void *buffe
     return err;
 }
 
+#if !CONFIG_OTA_ALLOW_HTTP
 static bool is_server_verification_enabled(esp_https_ota_config_t *ota_config) {
     return  (ota_config->http_config->cert_pem
             || ota_config->http_config->use_global_ca_store
-            || !(ota_config->http_config->crt_bundle_attach == NULL));
+            || ota_config->http_config->crt_bundle_attach != NULL);
 }
+#endif
 
 esp_err_t esp_https_ota_begin(esp_https_ota_config_t *ota_config, esp_https_ota_handle_t *handle)
 {