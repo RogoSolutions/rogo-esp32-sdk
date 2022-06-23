ccutrer/esp-idf

esp_prov: prompt for PoP if not provided and required

tools/esp_prov/esp_prov.py
@@ -384,8 +384,7 @@ async def main():
 
             if (args.secver != 0) and not await has_capability(obj_transport, 'no_pop'):
                 if len(args.pop) == 0:
-                    print('---- Proof of Possession argument not provided ----')
-                    exit(2)
+                    args.pop = binput('Proof of Possession required : ')
             elif len(args.pop) != 0:
                 print('---- Proof of Possession will be ignored ----')
                 args.pop = ''

RogoSolutions/rogo-esp32-sdk

tools/esp_prov/esp_prov.py
@@ -409,8 +409,7 @@ async def main():
 
             if (args.secver != 0) and not await has_capability(obj_transport, 'no_pop'):
                 if len(args.sec1_pop) == 0:
-                    print('---- Proof of Possession argument not provided ----')
-                    exit(2)
+                    args.sec1_pop = binput('Proof of Possession required : ')
             elif len(args.sec1_pop) != 0:
                 print('---- Proof of Possession will be ignored ----')
                 args.sec1_pop = ''